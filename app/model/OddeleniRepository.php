6022df1d9c0fd73601a563e41a0173b23a61ca97
jara <jaromirdeak@gmail.com>
2013-11-20 21:05:23 +0100

    hotova oprava ztracenych souboru ... az budu zase nekdy chtit poslat rm -rf na tridenni praci tak me radsi prizabijte :-)

--- app/model/OddeleniRepository.php
+++ app/model/OddeleniRepository.php
@@ -4,51 +4,14 @@ namespace Todo;
 
 use Nette;
 
-class UvazekRepository extends Repository {
+class OddeleniRepository extends Repository {
 
-    public function findByName($username) {
-        return $this->findAll()->where('username', $username)->fetch();
+    public function findPairsZkratkaOddNazev() {
+        return $this->getTable()->fetchPairs('zkratkaOdd', 'nazev');
     }
 
-    public function najdiUvazky($IDlekare) {
-        return $this->findAll()->where('IDlekare', $IDlekare)->fetchPairs('IDlekare', 'zkratkaOdd');
-        //return $this->findAll()->findBy(array('IDlekare' => $IDlekare))->fetchPairs('IDlekare', 'zkratkaOdd');
-    }
-
-    public function deleteZamestnanec($IDlekare) {
-        $this->findAll()->where('IDlekare', $IDlekare)->delete();
-    }
-
-    public function deleteUvazek($IDlekare, $zkratkaOdd) {
-        $this->findAll()->where('IDlekare', $IDlekare)->where('zkratkaOdd', $zkratkaOdd)->delete();
-    }
-
-    public function findByIDzamestnance($IDlekare) {
-        return $this->findAll()->where('IDlekare', $IDlekare);
-    }
-    
-//    public function findByIDzamestnance($IDlekare) {
-//        return $this->getTable()->where('oddeleni.zkratkaOdd = ', $value)->where('IDlekare', $IDlekare);
-//    }
-
-    public function findByIDzamestnanceZkratkaOdd($IDlekare, $zkratkaOdd) {
-        return $this->findAll()->where('IDlekare', $IDlekare)->where('zkratkaOdd', $zkratkaOdd)->fetch();
-    }
-
-    public function updateUvazek($IDlekare, $zkratkaOdd, $roleUvazku, $telefon) { // TODO opravit - nejede
-        $this->findAll()->where('IDlekare', $IDlekare)->where('zkratkaOdd', $zkratkaOdd)->update(
-                array('zkratkaOdd' => $zkratkaOdd,
-                    'roleUvazku' => $roleUvazku,
-                    'telefon' => $telefon));
-    }
-
-    public function addUvazek($IDlekare, $zkratkaOdd, $roleUvazku, $telefon) {
-        $this->getTable()->insert(
-                array(
-                    'IDlekare' => $IDlekare,
-                    'zkratkaOdd' => $zkratkaOdd,
-                    'roleUvazku' => $roleUvazku,
-                    'telefon' => $telefon));
+    public function functionName($param) {
+        
     }
 
 }
