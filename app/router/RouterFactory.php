3f016afc8f02acc26b68d5750849bea00d1df2f8
Jan Wrona <xwrona00@stud.fit.vutbr.cz>
2013-12-10 21:11:48 +0100

    Dokumentace

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -14,7 +14,7 @@ class RouterFactory {
      */
     public function createRouter() {
         $router = new RouteList();
-        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default', Route::SECURED);
         return $router;
     }
 
